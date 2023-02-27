477644e3ffa5cb7cfe181b2d45ddc969181cbcbe
winlin108 <winlin@gmail.com>
2023-02-26 19:23:58 -0600

    updated factory fixture for process_data and added to test functions; added related json_reader and cast_map_data functions

--- scripts/data_processor.py
+++ scripts/data_processor.py
@@ -1,15 +1,30 @@
 import csv
+import json
 
 
 def csv_reader(file_location):
     with open(file_location, mode='r') as csv_file:
         data = [line for line in csv.DictReader(csv_file)]
-        for row in data:
-            try:
-                row['Lat'] = float(row['Lat'])
-                row['Long'] = float(row['Long'])
-                row['Altitude'] = float(row['Altitude'])
-            except Exception as exp:
-                raise ValueError(str(exp))
+        data = cast_map_data(data)
 
-        return data
+    return data
+
+
+def json_reader(file_location):
+    with open(file_location, mode='r') as json_file:
+        data = [line for line in json.load(json_file)]
+        data = cast_map_data(data)
+
+    return data
+
+
+def cast_map_data(data):
+    for row in data:
+        try:
+            row['Lat'] = float(row['Lat'])
+            row['Long'] = float(row['Long'])
+            row['Altitude'] = float(row['Altitude'])
+        except Exception as exp:
+            raise ValueError(str(exp))
+
+    return data
